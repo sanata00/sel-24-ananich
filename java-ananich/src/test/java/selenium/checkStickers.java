39035a057fbc6ff76e94cc4df283bd78c1ec5b1c
pavel.ananich <pavel.ananich@capsilon.com>
2018-12-07 10:39:01 +0300

    checkStickers test is added

---
+++ java-ananich/src/test/java/selenium/checkStickers.java
@@ -0,0 +1,65 @@
+package selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class checkStickers {
+
+    private WebDriver driver;
+
+    @Rule
+    public ErrorCollector collector = new ErrorCollector();
+
+    @Before
+    public void start() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void clickAllSections() {
+
+        driver.get("http://localhost/litecart/en/");
+
+        //Click all categories
+        List<WebElement> categories = driver.findElements(By.cssSelector("nav.content > ul > li"));
+        int numberOfCategories = categories.size();
+
+        //Click categories in a loop
+        for (int i=1; i<(numberOfCategories+1); i++) {
+            driver.findElement(By.cssSelector("nav.content > ul > li:nth-child(" + i + ") a")).click();
+
+            //Check that each item has only one sticker
+            List<WebElement> items = driver.findElements(By.cssSelector("ul.products div.image-wrapper"));
+            for(WebElement item : items) {
+                List<WebElement> stickers = item.findElements(By.cssSelector("div"));
+                try {
+                    assertEquals(1, stickers.size());
+                } catch (Throwable t) {
+                    collector.addError(t);
+                }
+            }
+        }
+}
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
