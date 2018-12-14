f442c72908340ed635eb74c1ef47df6cbf1d52aa
sanata <pavel.ananich@gmail.com>
2018-12-14 22:47:34 +0300

    addNewProduct test is finished

--- java-ananich/src/test/java/selenium/lesson6/AddNewProduct.java
+++ java-ananich/src/test/java/selenium/lesson6/AddNewProduct.java
@@ -6,11 +6,11 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class AddNewProduct {
@@ -27,6 +27,8 @@ public class AddNewProduct {
 
     @Test
     public void addNewProduct() {
+        //Test Data
+        String productName = "Car Toy";
 
         //Login
         driver.get("http://localhost/litecart/admin/");
@@ -39,8 +41,8 @@ public class AddNewProduct {
         driver.findElement(By.cssSelector("td#content a:nth-child(2)")).click();
 
         //Populate 'General' tab
-        /*driver.findElement(By.cssSelector("div#tab-general tr:nth-of-type(1) label:nth-of-type(1) input")).click();
-        driver.findElement(By.cssSelector("input[name=name\\[en\\]]")).sendKeys("Car Toy");
+        driver.findElement(By.cssSelector("div#tab-general tr:nth-of-type(1) label:nth-of-type(1) input")).click();
+        driver.findElement(By.cssSelector("input[name=name\\[en\\]]")).sendKeys(productName);
         driver.findElement(By.cssSelector("input[name=code")).sendKeys("12345");
         driver.findElement(By.cssSelector("div#tab-general tr:nth-of-type(4) tr:nth-of-type(2) input")).click();
         new Select(driver.findElement(By.cssSelector("div#tab-general tr:nth-of-type(5) select")))
@@ -51,26 +53,35 @@ public class AddNewProduct {
         new Select(driver.findElement(By.cssSelector("select[name=delivery_status_id]")))
                 .selectByVisibleText("3-5 days");
         new Select(driver.findElement(By.cssSelector("select[name=sold_out_status_id]")))
-                .selectByVisibleText("-- Select --");*/
-        System.out.println("Working Directory = " +
-                System.getProperty("user.dir"));
+                .selectByVisibleText("-- Select --");
         driver.findElement(By.cssSelector("input[name=new_images\\[\\]]"))
                 .sendKeys(System.getProperty("user.dir") + "/src/test/resources/images/carToy.jpg");
+        driver.findElement(By.cssSelector("input[name=date_valid_from]"))
+                .sendKeys("1512" + Keys.TAB + "2018");
+        driver.findElement(By.cssSelector("input[name=date_valid_to]"))
+                .sendKeys("3112" + Keys.TAB + "2018");
 
+        //Populate 'Information' tab
+        driver.findElement(By.cssSelector("a[href*=tab-information]")).click();
+        new Select(driver.findElement(By.cssSelector("select[name=manufacturer_id]"))).selectByVisibleText("ACME Corp.");
+        driver.findElement(By.cssSelector("input[name=keywords]")).sendKeys("car toy boy");
+        driver.findElement(By.cssSelector("input[name=short_description\\[en\\]]")).sendKeys("very good car");
+        driver.findElement(By.cssSelector("div.trumbowyg-editor")).sendKeys("very good car long description");
+        driver.findElement(By.cssSelector("input[name=head_title\\[en\\]]")).sendKeys("SUPER CAR");
+        driver.findElement(By.cssSelector("input[name=meta_description\\[en\\]]")).sendKeys("meta description");
 
+        //Populate 'Prices' tab
+        driver.findElement(By.cssSelector("a[href*=tab-prices]")).click();
+        WebElement purchasePriceElement = driver.findElement(By.cssSelector("input[name=purchase_price]"));
+        purchasePriceElement.clear();
+        purchasePriceElement.sendKeys("15");
+        new Select(driver.findElement(By.cssSelector("select[name=purchase_price_currency_code]"))).selectByVisibleText("US Dollars");
+        driver.findElement(By.cssSelector("input[name=prices\\[USD\\]")).sendKeys("20");
+        driver.findElement(By.cssSelector("input[name=prices\\[EUR\\]")).sendKeys("18");
+        driver.findElement(By.cssSelector("button[name=save]")).click();
 
-
-
-
-
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
+        //Check that the product is created
+        driver.findElement(By.linkText(productName));
     }
 
     @After
